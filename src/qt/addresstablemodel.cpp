9de27fcd4d0a83ba95acd79bd0a22bfdd146fa3f
chromatic <chromatic@wgz.org>
2022-10-23 21:12:30 -0700

    std::stable_sort

--- src/qt/addresstablemodel.cpp
+++ src/qt/addresstablemodel.cpp
@@ -96,7 +96,7 @@ public:
         // qLowerBound() and qUpperBound() require our cachedAddressTable list to be sorted in asc order
         // Even though the map is already sorted this re-sorting step is needed because the originating map
         // is sorted by binary address, not by base58() address.
-        qSort(cachedAddressTable.begin(), cachedAddressTable.end(), AddressTableEntryLessThan());
+        std::stable_sort(cachedAddressTable.begin(), cachedAddressTable.end(), AddressTableEntryLessThan());
     }
 
     void updateEntry(const QString &address, const QString &label, bool isMine, const QString &purpose, int status)
